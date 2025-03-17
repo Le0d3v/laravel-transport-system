ba308ace83787f9110516e227e13e46374cb56bd
Leonardo Torres <harrywilsonth01@gmail.com>
2025-03-16 18:49:48 -0600

    Pruebas estaticas

---
+++ resources/views/public/footer.blade.php
@@ -0,0 +1,61 @@
+<div class="w-full bg-blue-500 py-5 px-16">
+  <div class="flex justify-between gap-10 mb-5">
+    <div class="w-1/2">
+      <h1 class="text-white text-2xl font-bold">
+        Busify
+      </h1>
+      <p class="text-white">
+        Hola ipsum, dolor sit amet consectetur adipisicing elit. Vero aliquam laudantium autem ab. Molestias nulla soluta sapiente, error mollitia quisquam perferendis, eos voluptates suscipit eligendi libero cupiditate perspiciatis expedita nihil?
+      </p>
+    </div>
+    <div class="w-1/2 text-end text-white">
+      <h1 class="text-white text-2xl font-bold text-end">
+        Contactanos
+      </h1>
+      <p class="text-xl mt-2">
+        <i class="fa-solid fa-phone text-emerald-500"></i> 
+        2217788382
+      </p>
+      <div class="flex justify-end">
+        <div class="text-xl text-emerald-400 flex gap-10 mt-5 ">
+          <a href="#" class="text-xl">
+            <i class="fa-brands fa-facebook w-16"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-instagram text-xl"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-whatsapp text-xl"></i>
+          </a>
+          <a href="#">
+            <i class="fa-brands fa-tiktok text-xl"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="flex gap-16 mt-5">
+    <div class="w-1/3">
+      <p class="text-white text-sm">
+        2025 Autobuses en todo México. S.A de C.V. Todos los derechos reservados. Boletos de Autobús, viaja por todo México. Diseñado y Desarrollado por InnovaTech v.2.0.1
+      </p>
+    </div>
+    <div class="flex justify-between w-2/3">
+      <a href="#" class="text-white">
+        Terminos y Condiciones
+      </a>
+      <a href="#" class="text-white">
+        Aviso de Privacidad
+      </a>
+      <a href="#" class="text-white">
+        Políticas de viaje
+      </a>
+      <a href="#" class="text-white">
+        Aviso de Privacidad 
+        <br>
+        <p>Colaboradores</p>
+      </a>
+    </div>
+  </div>
+</div>
